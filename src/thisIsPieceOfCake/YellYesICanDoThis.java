b07e4c42e454eab78da6f9e3c3e10357b0a88d49
panthersClass <rezarahmanian@tekschool.us>
2022-11-13 21:56:30 -0500

    this is for branching purpose

---
+++ src/thisIsPieceOfCake/YellYesICanDoThis.java
@@ -0,0 +1,11 @@
+package thisIsPieceOfCake;
+
+public class YellYesICanDoThis {
+	
+	public void icanLearnGit() {
+		// TODO Auto-generated method stub
+        System.out.println("I Learn this or i will fast for rest of the night");
+	}
+
+}
+
